d23047fb44abd8f7b4f361e82f2cf79aaf479a29
NithishaSathishkumar <nithisha132004@gmail.com>
2025-04-25 20:28:10 -0700

    login intitial

--- Hackthon2025/App.js
+++ Hackthon2025/App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import Login from './Screen /login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+       <Login/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
